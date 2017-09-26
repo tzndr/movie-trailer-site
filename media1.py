9d1e646dcdb8045e24535e12a88a83d2293e5489
tzndr <32205118+tzndr@users.noreply.github.com>
2017-09-26 10:30:39 -0500

    Update media1.py

--- media1.py
+++ media1.py
@@ -5,7 +5,7 @@ class Movie():
     """ This class stores movie information"""
 
     def __init__(self, movie_title, poster_image, trailer_youtube):
-        """These methods allow for each movie to display its unique info"""
+        """ These methods allow for each movie to display its unique info"""
         self.title = movie_title
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
