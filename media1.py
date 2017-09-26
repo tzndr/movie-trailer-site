ac4141bd9cce2ac6ef890a59ddee77f62d26c15b
tzndr <32205118+tzndr@users.noreply.github.com>
2017-09-26 10:28:34 -0500

    Add files via upload

--- media1.py
+++ media1.py
@@ -1,10 +1,15 @@
 import webbrowser
 
-class Movie():    
+
+class Movie():
+    """ This class stores movie information"""
+
     def __init__(self, movie_title, poster_image, trailer_youtube):
+        """These methods allow for each movie to display its unique info"""
         self.title = movie_title
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
-
+       
     def show_trailer(self):
+        """ This function plays the movie's trailer once clicked"""
         webbrowser.open(self.trailer_youtube_url)
